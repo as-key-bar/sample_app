64fd6a593c8b8e8d79b9eff069e46beba620ed06
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-06-18 15:35:45 +0900

    8.1 session用のcontrollerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root "static_pages#home"
   get  "/help",    to: "static_pages#help"
@@ -9,5 +10,8 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
   resources :users
 end
