3f72c343b20947dbc34c8337f214dee6516cdc43
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-06-18 20:35:51 +0900

    テストを修正

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -35,7 +35,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
     follow_redirect!
     # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
-    delete logout_path    
+    delete logout_path
+    follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
