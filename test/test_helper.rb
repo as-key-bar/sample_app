51c65455a01391423caeb6e5493951499991627e
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-06-18 20:11:53 +0900

    9.3 rememberのテスト追加

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,22 +18,12 @@ class ActiveSupport::TestCase
   
   # Add more helper methods to be used by all tests here...
 
-class ActiveSupport::TestCase
-  # 指定のワーカー数でテストを並列実行する
-  parallelize(workers: :number_of_processors)
-  # test/fixtures/*.ymlのfixtureをすべてセットアップする
-  fixtures :all
-
-  # テストユーザーがログイン中の場合にtrueを返す
-  def is_logged_in?
-    !session[:user_id].nil?
-  end
-
   # テストユーザーとしてログインする
   def log_in_as(user)
     session[:user_id] = user.id
   end
 end
+
 
 class ActionDispatch::IntegrationTest
 
