7eadcd123bcf67a0b0742d71bfaef6ae98f19159
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-06-19 20:52:57 +0900

    set app url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,19 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://sample_app_askey.onrender.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :port           => 2525,
+    :address        => 'smtp.mailgun.org',
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
+    :authentication => :plain,
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
