442de310a3c38ef20648245c060401d9b2437c64
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-06-23 19:35:42 +0900

    switch storage to localstack

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # Store uploaded files on LocalStack S3 for development (see config/storage.yml).
+  config.active_storage.service = :localstack
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
