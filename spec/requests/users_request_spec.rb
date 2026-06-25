c7fcdf4bf2f3f517f1ca2231da8a038d4fa9b673
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-06-25 15:23:00 +0900

    fix all controller test

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe UsersController, type: :request do
+  fixtures :users
+
   let(:user) { users(:michael) }
   let(:other_user) { users(:archer) }
 
@@ -10,16 +12,16 @@ RSpec.describe UsersController, type: :request do
   end
 
   it "should redirect edit when logged in as wrong user" do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
+    log_in_as(other_user)
+    get edit_user_path(user)
     expect(flash).to be_empty
     expect(response).to redirect_to(root_url)
   end
 
   it "should redirect update when logged in as wrong user" do
-    log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
+    log_in_as(other_user)
+    patch user_path(user), params: { user: { name: user.name,
+                                              email: user.email } }
     expect(flash).to be_empty
     expect(response).to redirect_to(root_url)
   end
@@ -30,12 +32,12 @@ RSpec.describe UsersController, type: :request do
   end
 
     it "should redirect following when not logged in" do
-    get following_user_path(@user)
+    get following_user_path(user)
     expect(response).to redirect_to(login_url)
   end
 
   it "should redirect followers when not logged in" do
-    get followers_user_path(@user)
+    get followers_user_path(user)
     expect(response).to redirect_to(login_url)
   end
 end
