3806d80ec728f2d83f038c6fa16a34289a314950
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-01 14:28:17 +0900

    add mute and block test

--- spec/models/block_spec.rb
+++ spec/models/block_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Block, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :users
+  block = Block.new(blocker_id: users(:michael).id, blocked_id: users(:archer).id)
+
+  it "should be valid" do
+    expect(block).to be_valid
+  end
+
+  it "should require a blocker_id" do
+    block.blocker_id = nil
+    expect(block).not_to be_valid
+  end
+
+  it "should require a blocked_id" do
+    block.blocked_id = nil
+    expect(block).not_to be_valid
+  end
+
 end
--- spec/models/mute_spec.rb
+++ spec/models/mute_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Mute, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :users
+  mute = Mute.new(muter_id: users(:michael).id, muted_id: users(:archer).id)
+
+  it "should be valid" do
+    expect(mute).to be_valid
+  end
+
+  it "should require a muter_id" do
+    mute.muter_id = nil
+    expect(mute).not_to be_valid
+  end
+
+  it "should require a muted_id" do
+    mute.muted_id = nil
+    expect(mute).not_to be_valid
+  end
+
 end
