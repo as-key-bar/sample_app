a4a2aea130e84d46c5f5b648803e0290dbbc7338
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-02 20:38:26 +0900

    add controller

--- app/controllers/block_controller.rb
+++ app/controllers/block_controller.rb
@@ -1,2 +1,16 @@
-class BlockController < ApplicationController
+class BlocksController < ApplicationController
+
+  def create
+    user = User.find(params[:blocked_id])
+    current_user.block(user)
+    redirect_to user
+  end
+
+  def destroy
+    block = current_user.active_blocks.find(params[:id])
+    user = block.blocked
+    current_user.unblock(user)
+    redirect_to user, status: :see_other
+  end
+
 end
--- app/controllers/mutes_controller.rb
+++ app/controllers/mutes_controller.rb
@@ -1,2 +1,14 @@
 class MutesController < ApplicationController
+  def create
+    user = User.find(params[:muted_id])
+    current_user.mute(user)
+    redirect_to user
+  end
+
+  def destroy
+    mute = current_user.active_mutes.find(params[:id])
+    user = mute.muted
+    current_user.unmute(user)
+    redirect_to user, status: :see_other
+  end
 end
