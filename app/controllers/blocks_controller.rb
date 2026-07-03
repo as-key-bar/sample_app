621e247118c1abfc3e42b3a9ff4ce09aff4d8c21
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-03 21:25:06 +0900

    unfollow target user when block

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -4,6 +4,7 @@ class BlocksController < ApplicationController
     user = User.find(params[:blocked_id])
     current_user.block(user)
     user.unfollow(current_user) if user.following?(current_user)
+    current_user.unfollow(user) if current_user.following?(user)
     redirect_back(fallback_location: root_path) 
     # redirect_to user
   end
