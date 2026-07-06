13b93b92ff371d69b943c220cd6fa1b6bdcaaceb
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-06 16:51:42 +0900

    add block request test

---
+++ spec/requests/block_request_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Relationships", type: :request do
+  fixtures :users, :relationships
+
+  let(:user) { users(:michael) }
+  let(:other_user) { users(:archer) }
+
+  describe "POST /block" do
+    context "ログインしている場合" do
+      before do
+        log_in_as(user)
+      end
+
+      it "ブロックできるか" do
+        expect {
+          post blocks_path, params: { blocked_id: other_user.id }
+        }.to change(Block, :count).by(1)
+
+      end
+    end
+  end
+
+  describe "DELETE /block/:id" do
+    let!(:block) { user.active_blocks.create!(blocked_id: other_user.id) }
+    context "ログインしている場合" do
+      before do
+        log_in_as(user)
+      end
+
+      it "ブロック解除できること" do
+        expect {
+          delete block_path(block)
+        }.to change(Block, :count).by(-1)
+      end
+    end
+  end
+end
