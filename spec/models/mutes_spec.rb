7c9014b42e8913d80fceeb2bd33db7ad58272c50
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-08 19:14:31 +0900

    fix mute test when not logined

---
+++ spec/models/mutes_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Mute, type: :model do
+
+  fixtures :users
+  let(:mute) { Mute.new(muter_id: users(:michael).id, muted_id: users(:archer).id) }
+
+  it "should be valid" do
+    expect(mute).to be_valid
+  end
+
+  it "should require a muter_id" do
+    mute.muter_id = nil
+    expect(mute).not_to be_valid
+  end
+
+  it "should require a muted_id" do
+    mute.muted_id = nil
+    expect(mute).not_to be_valid
+  end
+
+end
