2232336683e8bfb9e5374bb05e3b9aa753b4e985
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-09 15:14:40 +0900

    move  block test from mute

--- spec/requests/block_request_spec.rb
+++ spec/requests/block_request_spec.rb
@@ -25,8 +25,43 @@ RSpec.describe "Relationships", type: :request do
         expect {
           post blocks_path, params: { blocked_id: other_user.id }
         }.to change(Block, :count).by(1)
-
       end
+
+      let(:invalid_user_id) { 0 }
+
+
+      it "存在しないアカウントへのブロック" do
+        expect {
+          post blocks_path, params: { blocked_id: invalid_user_id }        
+        }.to change(Block, :count).by(0)
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it "自分自身をブロックできない" do
+        expect {
+          post blocks_path, params: { blocked_id: user.id }
+        }.not_to change(Block, :count)
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it "不正な文字列をIDとして渡した場合ブロックできない" do
+        expect {
+          post blocks_path, params: { blocked_id: "invalid" }
+        }.not_to change(Block, :count)
+        expect(response).to have_http_status(:bad_request)
+      end
+
+
+      it "すでにブロックしているユーザーを再度ブロックできない" do
+        user.block(other_user) 
+        expect {
+          post blocks_path,
+              params: { blocked_id: other_user.id },
+              headers: { "HTTP_REFERER" => user_path(other_user) }
+        }.not_to change(Block, :count)
+
+        expect(response).to redirect_to(user_path(other_user))      
+      end      
     end
   end
 
