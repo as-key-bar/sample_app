a929a214766fcaf323b0b672c39bdb6a00ace5aa
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-10 20:04:26 +0900

    mute controller refactor

--- app/controllers/mutes_controller.rb
+++ app/controllers/mutes_controller.rb
@@ -1,26 +1,20 @@
 class MutesController < ApplicationController
   before_action :logged_in_user
+
   def create
-    if !current_user.muting?(@user)
-      unless !User.exists?(params[:muted_id]) || current_user.id == params[:muted_id].to_i
-        user = User.find(params[:muted_id])
-        current_user.mute(user)
-        # redirect_to user
-        redirect_back(fallback_location: root_path) 
-      else
-        redirect_to root_path, status: :bad_request
-      end
+    if User.exists?(params[:muted_id]) && current_user.id != params[:muted_id].to_i
+      user = User.find(params[:muted_id])
+      current_user.mute(user)
+      redirect_back(fallback_location: root_path) 
+    else
+      redirect_to root_path, status: :bad_request
     end
   end
 
   def destroy
-    mute = current_user.active_mutes.find_by(id: params[:id])
-
-    unless mute.nil?
+    if mute = current_user.active_mutes.find_by(id: params[:id])
       target_user = mute.muted
-      if current_user.muting?(target_user)
-        current_user.unmute(target_user)
-      end
+      current_user.unmute(target_user)
     end
     redirect_back(fallback_location: root_path)
   end
