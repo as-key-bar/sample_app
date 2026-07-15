d858f321c97e81e01c9a0bf016698ecdf3a80ef9
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-15 17:26:42 +0900

    add search result template and view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,16 @@
 class SearchController < ApplicationController
   def search
-    # @query = params[:query]
-    @title = "Search"
-    render 'search'
+    if @query = params[:query]
+      search_results
+    else
+      @title = "Search"
+     render 'search'
+    end
   end
 
-  # def search_results
-  #   @query = params[:query]
-  #   render 'search_results'
-  # end
+  def search_results
+    @title = "Search Results of #{@query}"
+    render 'search_result'
+    @microposts = Micropost.find_by(id: 1)
+  end
 end
