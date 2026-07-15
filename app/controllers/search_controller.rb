beaa0f26b0258fcdbef9799ae59c34f961f6ee66
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-15 18:28:08 +0900

    add dmy data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,7 +10,18 @@ class SearchController < ApplicationController
 
   def search_results
     @title = "Search Results of #{@query}"
+    @microposts_dmy = [
+      Micropost.new(id: 1, content: "これはテスト投稿1です。", user_id: 1, created_at: Time.current),
+      Micropost.new(id: 2, content: "これはテスト投稿2です。", user_id: 1, created_at: Time.current),
+      Micropost.new(id: 3, content: "これはテスト投稿3です。", user_id: 1, created_at: Time.current)
+    ]
+
+
+    @microposts = WillPaginate::Collection.create(1, 10, @microposts_dmy.length) do |pager|
+      pager.replace(@microposts_dmy)
+    end
+
     render 'search_result'
-    @microposts = Micropost.find_by(id: 1)
+
   end
 end
