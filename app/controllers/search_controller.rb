f322575a91749d6f16f8fe7ed89dcde316844b36
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-16 12:08:26 +0900

    add minimum search method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,18 +10,8 @@ class SearchController < ApplicationController
 
   def search_results
     @title = "Search Results of #{@query}"
-    @microposts_dmy = [
-      Micropost.new(id: 1, content: "これはテスト投稿1です。", user_id: 1, created_at: Time.current),
-      Micropost.new(id: 2, content: "これはテスト投稿2です。", user_id: 1, created_at: Time.current),
-      Micropost.new(id: 3, content: "これはテスト投稿3です。", user_id: 1, created_at: Time.current)
-    ]
-
-
-    @microposts = WillPaginate::Collection.create(1, 10, @microposts_dmy.length) do |pager|
-      pager.replace(@microposts_dmy)
-    end
+    @microposts = Micropost.where("content LIKE ?", "%#{@query}%").paginate(page: params[:page], per_page: 10)
 
     render 'search_result'
-
   end
 end
