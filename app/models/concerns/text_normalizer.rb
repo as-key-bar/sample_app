11b07daa7f3e51c32c0944a7bab5caa2b3975965
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-17 21:43:50 +0900

    読み方生成のコード作成　意味の理解がまだ不十分なので後で読み直す

---
+++ app/models/concerns/text_normalizer.rb
@@ -0,0 +1,15 @@
+# app/models/concerns/string_normalizer.rb
+require 'nkf'
+
+module StringNormalizer
+  extend ActiveSupport::Concern
+  module_helper = Module.new do
+    def convert_to_hiragana(text)
+      return "" if text.blank?
+      NKF.nkf('-w -W -h1 -x', text)
+    end
+  end
+  
+  extend module_helper
+  include module_helper
+end
