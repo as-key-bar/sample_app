4c68e3b1933c127c707e6b9b70b5bc5d3943d91c
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-27 15:24:04 +0900

    実装完了

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,20 +6,31 @@ class SearchController < ApplicationController
       search_results
     else
       @title = "Search"
-     render 'search'
+      @follow_only = params[:follow_only]
+      render 'search'
     end
   end
 
   def search_results
     return if !@query 
     @title = "Search Results of #{@query}"
-
     queries = @query.strip.split(/[[:blank:]]+/)
+    @follow_only = params[:follow_only]
 
+    if current_user
+      target_user_ids = current_user.following.map(&:id)
+      target_user_ids << current_user.id
+    end
+    
     scope = Micropost.all
     @microposts = queries.reduce(scope) do |result, q|
       searchkey = convert_to_searchkey(q)
-      result.where("searchkey LIKE ? OR searchkey LIKE ?", "%#{searchkey}%", "%#{q}%")
+      if current_user && @follow_only == "1"
+        result.where(user_id: target_user_ids)
+              .where("searchkey LIKE ? OR searchkey LIKE ?", "%#{searchkey}%", "%#{q}%")
+      else
+        result.where("searchkey LIKE ? OR searchkey LIKE ?", "%#{searchkey}%", "%#{q}%")
+      end
     end.paginate(page: params[:page], per_page: 10)
 
     render 'search_result'
