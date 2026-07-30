0a22c870b4cbebbfa8c2a7c41e327a8290193e53
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-30 11:04:09 +0900

    fix css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_07_29_070555) do
+ActiveRecord::Schema[7.0].define(version: 2026_07_29_055036) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -84,7 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_07_29_070555) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at", null: false
+    t.datetime "created_at", null: famicrolse
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
