bdbe1c4250ca5f4ed9fa0f3c1449ae05571c23cb
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-07-31 15:20:38 +0900

    delete unuse method

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -14,11 +14,4 @@ class Micropost < ApplicationRecord
                                       message: "must be a valid image format" },
                       size: { less_than: 5.megabytes,
                               message:   "should be less than 5MB" }
-
-  def set_reply_to(micropost)
-    return false if micropost.nil?
-    self.reply_to = micropost
-    self.save
-  end
-
 end
