e7b8011ef4f539c844e9cfa7a895464528f24330
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-08-05 15:55:26 +0900

    fix controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,9 @@
 class NotificationsController < ApplicationController
   def index
+    if !current_user.nil?
+      @notifications = current_user.notifications.order(created_at: :desc)
+    else 
+      redirect_to root_url
+    end
   end
 end
