3faab7c28b38a010bed88ad0c9d882b7c962796e
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-08-05 19:17:59 +0900

    change block logic

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,11 @@
 class NotificationsController < ApplicationController
   def index
     if !current_user.nil?
-      @notifications = current_user.notifications.order(created_at: :desc)
+      blocking_ids = current_user.blocking.pluck(:id)
+      @notifications = current_user.notifications
+                                    .includes(:notifiable)
+                                    .order(created_at: :desc)
+                                    .reject { |notification| blocking_ids.include?(notification.sender&.id) }
     else 
       redirect_to root_url
     end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,13 @@
 class Notification < ApplicationRecord
   belongs_to :notifiable, polymorphic: true
   belongs_to :user
+
+  def sender
+    case notifiable
+    when Relationship
+      notifiable.follower
+    when Micropost
+      notifiable.user
+    end
+  end
 end
