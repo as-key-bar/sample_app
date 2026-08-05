ba1dc09866485beea0a9e6479590209d3cc5434b
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-08-05 15:56:38 +0900

    fix spec

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Notifications", type: :request do
+  fixtures :users
+  let(:user) { users(:michael) }
+
   describe "GET /index" do
+    before do
+      log_in_as(user)
+    end
     it "returns http success" do
-      get "/notifications/index"
+      get "/notifications"
       expect(response).to have_http_status(:success)
     end
   end
