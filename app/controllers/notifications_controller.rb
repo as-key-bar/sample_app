236c72b7766d68b73dd84f2c3b232d11896f8f11
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-08-06 16:05:34 +0900

    add pagenate to notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,10 +2,16 @@ class NotificationsController < ApplicationController
   def index
     if !current_user.nil?
       blocking_ids = current_user.blocking.pluck(:id)
+      blocked_relationship_ids = Relationship.where(follower_id: current_user.blocking.select(:id)).select(:id)
+      blocked_micropost_ids    = Micropost.where(user_id: current_user.blocking.select(:id)).select(:id)
+
       @notifications = current_user.notifications
                                     .includes(:notifiable)
+                                    .where.not(notifiable_type: "Relationship", notifiable_id: blocked_relationship_ids)
+                                    .where.not(notifiable_type: "Micropost",    notifiable_id: blocked_micropost_ids)
                                     .order(created_at: :desc)
-                                    .reject { |notification| blocking_ids.include?(notification.sender&.id) }
+                                    .paginate(page: params[:page], per_page: 10)
+
     else
       redirect_to root_url
     end
