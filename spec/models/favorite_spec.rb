b524f3adafb52759b0068b09585e6a042d6541b0
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-08-10 18:51:34 +0900

    add model spec

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :users, :microposts
+
+  let(:favorite) do
+    Favorite.new(
+      favoriter_id: users(:michael).id,
+      favorited_id: microposts(:orange).id
+    )
+  end
+
+  context "Favorite Model Spec" do
+  it "validが通るかどうか" do
+    expect(favorite).to be_valid
+  end
+
+  it "ユーザーが空の時を弾くかどうか" do
+    favorite.favoriter_id = nil
+    expect(favorite).not_to be_valid
+  end
+
+  it "お気に入りの投稿が空の時を弾くかどうか" do
+    favorite.favorited_id = nil
+    expect(favorite).not_to be_valid
+  end
 end
