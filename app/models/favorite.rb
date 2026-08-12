1433e1cba48879219e636d6b180c810f2ceb8184
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-08-12 19:24:06 +0900

    add fav notification

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,11 +1,14 @@
 class Favorite < ApplicationRecord
-  # include Notifiable
+  include Notifiable
   belongs_to :favoriter, class_name: "User"
   belongs_to :favorited, class_name: "Micropost"
   validates :favoriter_id, presence: true
   validates :favorited_id, presence: true
 
-  # def notification_recipient
-  #   favorited.user
-  # end
+  def notification_recipient
+    favorited.user
+  end
+  def denied?
+    favoriter == favorited.user
+  end
 end
