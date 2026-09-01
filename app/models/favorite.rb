66d1e9fa555d95e1ce305231059722748674aa19
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-09-01 21:02:57 +0900

    fix favorite model

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -4,6 +4,7 @@ class Favorite < ApplicationRecord
   belongs_to :favorited, class_name: "Micropost"
   validates :favoriter_id, presence: true
   validates :favorited_id, presence: true
+  validate :favorited_post_not_blocked, if: -> { favoriter && favorited }
 
   def notification_recipient
     favorited.user
@@ -11,4 +12,16 @@ class Favorite < ApplicationRecord
   def denied?
     favoriter == favorited.user
   end
+
+  def actor
+    favoriter
+  end
+
+  private
+
+    def favorited_post_not_blocked
+      return unless favoriter.interaction_blocked_with?(favorited.user)
+
+      errors.add(:favorited, "cannot favorite a blocked user's post")
+    end
 end
