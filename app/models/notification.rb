e910cdd8a00d57759dd521de17ffd62a83f6a82b
yuki.iriyama <iriyama-y@dwango.co.jp>
2026-09-01 20:59:21 +0900

    fix notification model to use actor

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,15 +1,5 @@
 class Notification < ApplicationRecord
   belongs_to :notifiable, polymorphic: true
   belongs_to :user
-
-  def sender
-    case notifiable_type
-    when "Relationship"
-      notifiable.follower
-    when "Micropost"
-      notifiable.user
-    when "Favorite"
-      notifiable.favoriter
-    end
-  end
+  belongs_to :actor, class_name: "User"
 end
